95398fbc3dd8643d9eb30d37e410041c12d24a1a
abdulsalam <olawoyinabadulsalam202@gmail.com>
2026-05-11 20:13:09 +0100

    fix: update cookie settings for cross-site

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,10 +12,10 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
-    httpOnly: true, //prevent xss attacks: cross site scripting
-    sameSite: "strict", //CSRF attack: cross site request forgery
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",   // changed from "strict"
+    secure: true,       // must be true when sameSite is "none"
   });
 
   return token;
